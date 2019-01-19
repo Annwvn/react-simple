8d2fe38859612c190c59e4e21103fe93248d0966
杨柳 <144844097541@qq.com>
2019-01-19 11:47:56 +0800

    one

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 class Header extends React.Component {
 	
     render() {
-        return <div className="">
-            header            
-    	</div>
+        return<div>
+        header
+        </div>
     }
 }
 
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,7 +2,7 @@ import Reducer from './action/rank'
 import thunk from 'redux-thunk'
 import {createStore,applyMiddleware,combineReducers} from 'redux'
 
-// let reducer = combineReducers({
+// let reducers = combineReducers({
 //     Reducer
 // })
 
